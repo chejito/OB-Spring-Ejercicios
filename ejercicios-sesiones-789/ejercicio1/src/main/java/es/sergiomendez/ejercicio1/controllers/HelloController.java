37659d93eae8b0ef65ce3020a59584af127de568
chejito <smndz81@gmail.com>
2021-10-21 12:28:35 +0200

    Ejercicio 1 sesiones 7, 8, y 9

---
+++ ejercicios-sesiones-789/ejercicio1/src/main/java/es/sergiomendez/ejercicio1/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package es.sergiomendez.ejercicio1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "¡¡Buenas!!";
+    }
+
+
+
+}
